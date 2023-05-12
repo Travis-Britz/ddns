a3de125c1031e3be96a79195027d5a282b60e409
Travis Britz <Travis-Britz@users.noreply.github.com>
2023-05-12 05:43:49 -0500

    include ipv6 addresses functioning DNS updates verbose logging

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/fs"
 	"log"
 	"net"
 	"net/netip"
 	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
 	"time"
 
@@ -20,17 +22,27 @@ import (
 )
 
 var config = struct {
-	Domain  string
-	KeyFile string
-	IP      string
+	Domain   string
+	KeyFile  string
+	IP       string
+	Interval time.Duration
+	Verbose  bool
 }{}
 
 func init() {
 	flag.StringVar(&config.Domain, "d", config.Domain, "DNS entry to update")
 	flag.StringVar(&config.IP, "ip", config.Domain, "IP address to set")
 	flag.StringVar(&config.KeyFile, "k", filepath.Join(os.Getenv("HOME"), ".cloudflare"), "Path to cloudflare API credentials file")
+	flag.DurationVar(&config.Interval, "i", 1*time.Minute, "Duration to wait between IP checks")
+	flag.BoolVar(&config.Verbose, "v", false, "Enable verbose logging")
 	flag.Parse()
+
+	if config.Verbose {
+		logger = log.Default()
+	}
 }
+
+var logger *log.Logger = log.New(io.Discard, "", log.LstdFlags)
 
 func main() {
 	if err := run(); err != nil {
@@ -43,16 +55,19 @@ func run() error {
 	if err := validate(); err != nil {
 		return err
 	}
+	logger.Printf("config is valid: %+v", config)
 
 	key, err := readKey(config.KeyFile)
 	if err != nil {
 		return fmt.Errorf("error reading key: %w", err)
 	}
+	logger.Println("successfully read key from key file")
 
-	newIPs, err := onlyIPv4(getLocalIPAddresses())
+	newIPs, err := getLocalIPAddresses()
 	if err != nil {
 		return fmt.Errorf("error getting local IPs: %w", err)
 	}
+	logger.Printf("got local IPs: %+v\n", newIPs)
 
 	api, err := cloudflare.NewWithAPIToken(key)
 	if err != nil {
@@ -65,50 +80,87 @@ func run() error {
 
 	return nil
 }
-
+func recordType(a netip.Addr) string {
+	if a.Is4() {
+		return "A"
+	}
+	if a.Is6() {
+		return "AAAA"
+	}
+	panic("unknown ip configuration")
+}
 func setIPs(api *cloudflare.API, domain string, addrs []netip.Addr) error {
-	return nil
-	// zid, err := api.ZoneIDByName(domain)
-	// if err != nil {
-	// 	return fmt.Errorf("could not find zone \"%s\" by name: %w", domain, err)
-	// }
-	// api.UpdateDNSRecord(context.TODO(), cloudflare.ZoneIdentifier(zid), cloudflare.UpdateDNSRecordParams{
-	// 	Type:     "",
-	// 	Name:     "",
-	// 	Content:  "",
-	// 	Data:     nil,
-	// 	ID:       "",
-	// 	Priority: new(uint16),
-	// 	TTL:      0,
-	// 	Proxied:  new(bool),
-	// 	Comment:  "",
-	// 	Tags:     []string{},
-	// })
 
-	// api.CreateDNSRecord(context.TODO(), cloudflare.ZoneIdentifier(zid), cloudflare.CreateDNSRecordParams{
-	// 	CreatedOn:  time.Time{},
-	// 	ModifiedOn: time.Time{},
-	// 	Type:       "",
-	// 	Name:       "",
-	// 	Content:    "",
-	// 	Meta:       nil,
-	// 	Data:       nil,
-	// 	ID:         "",
-	// 	ZoneID:     "",
-	// 	ZoneName:   "",
-	// 	Priority:   new(uint16),
-	// 	TTL:        0,
-	// 	Proxied:    new(bool),
-	// 	Proxiable:  false,
-	// 	Locked:     false,
-	// 	Comment:    "",
-	// 	Tags:       []string{},
-	// })
+	sl := strings.Split(domain, ".")
+	zone := strings.Join(sl[len(sl)-2:], ".")
+	logger.Printf("looking up zone ID for %s...\n", zone)
+	zid, err := api.ZoneIDByName(zone)
+	if err != nil {
+		return fmt.Errorf("unable to get zone ID for %s: %w", zone, err)
+	}
+	logger.Printf("got zone ID: %s\n", zid)
+	logger.Printf("looking up A,AAAA records for zone %s...\n", zid)
+
+	records, _, err := api.ListDNSRecords(context.Background(), cloudflare.ZoneIdentifier(zid), cloudflare.ListDNSRecordsParams{
+		Type:    "A,AAAA",
+		Name:    domain,
+		Content: "",
+		Comment: "",
+	})
+	logger.Printf("found %d existing records: %+v\n", len(records), records)
+	existing := map[netip.Addr]bool{}
+	newAddrs := map[netip.Addr]bool{}
+
+	for _, a := range addrs {
+		newAddrs[a] = true
+	}
+	for _, r := range records {
+		a, err := netip.ParseAddr(r.Content)
+		if err != nil {
+			return fmt.Errorf("error parsing IP from content: %w", err)
+		}
+		existing[a] = true
+
+		if _, found := newAddrs[a]; found {
+			logger.Printf("existing record %s is in the set of new addrs\n", a)
+			continue
+		}
+
+		logger.Printf("deleting DNS record for %s...\n", a)
+		err = api.DeleteDNSRecord(context.TODO(), cloudflare.ZoneIdentifier(zid), r.ID)
+		if err != nil {
+			return fmt.Errorf("unable to delete DNS record %s: %w", r.ID, err)
+		}
+		logger.Printf("successfully deleted record for %s\n", a)
+	}
+
+	for _, a := range addrs {
+		if _, found := existing[a]; found {
+			logger.Printf("record already exists for %s\n", a)
+			continue
+		}
+		logger.Printf("creating record for %s...", a)
+		record, err := api.CreateDNSRecord(context.TODO(), cloudflare.ZoneIdentifier(zid), cloudflare.CreateDNSRecordParams{
+			Type:    recordType(a),
+			Name:    domain,
+			Content: a.String(),
+			ZoneID:  zid,
+			TTL:     60,
+			Comment: "managed by ddns",
+		})
+		if err != nil {
+			return fmt.Errorf("error creating DNS record: %w", err)
+		}
+		logger.Printf("successfully added record: %+v\n", record)
+	}
+
+	return nil
 }
 
 func runSetup() error {
-
-	fmt.Print("Enter Cloudflare API Key: ")
+	logger.Println("running setup")
+	time.Sleep(200 * time.Millisecond)
+	fmt.Printf("Enter Cloudflare API Key: \n")
 	bytekey, err := term.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 		return fmt.Errorf("error reading from stdin: %w", err)
@@ -121,6 +173,7 @@ func runSetup() error {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+	logger.Println("verifying token...")
 	result, err := api.VerifyAPIToken(ctx)
 	if err != nil {
 		return fmt.Errorf("unable to verify api token: %w", err)
@@ -128,14 +181,16 @@ func runSetup() error {
 	if result.Status != "active" {
 		return fmt.Errorf("expected api token status to be \"active\"; got \"%s\"", result.Status)
 	}
+	logger.Println("token verified successfully")
 
+	logger.Printf("creating key file at \"%s\"\n", config.KeyFile)
 	f, err := os.OpenFile(config.KeyFile, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0600)
 	if err != nil {
 		return fmt.Errorf("unable to create \"%s\": %w", config.KeyFile, err)
 	}
 	defer f.Close()
 	fmt.Fprintln(f, key)
-
+	logger.Printf("token written to \"%s\"\n", config.KeyFile)
 	return nil
 }
 
@@ -168,8 +223,13 @@ func validate() error {
 		return errors.New("error: domain cannot be empty")
 	}
 
+	if !strings.Contains(config.Domain, ".") {
+		return errors.New("error: domain must have at least one dot")
+	}
+
 	_, err := os.Stat(config.KeyFile)
 	if os.IsNotExist(err) {
+		logger.Printf("key file \"%s\" does not exist\n", config.KeyFile)
 		if err := runSetup(); err != nil {
 			return fmt.Errorf("setup: %w", err)
 		}
