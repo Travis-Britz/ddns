8dae9b9c744e05515e6a9d6531951ecb5d5fb2c2
Travis Britz <Travis-Britz@users.noreply.github.com>
2023-05-31 22:40:38 -0500

    clean up examples

--- example_test.go
+++ example_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/netip"
 	"os"
 	"time"
 
@@ -16,7 +17,7 @@ func ExampleNew() {
 		"dynamic-local-ip.example.com",
 		ddns.NewCloudflare(os.Getenv("CLOUDFLARE_ZONE_TOKEN")),
 		ddns.UsingResolver(ddns.InterfaceResolver("eth0")),
-		ddns.WithLogger(log.New(io.Discard, "", log.LstdFlags)),
+		ddns.WithLogger(log.New(io.Discard, "", 0)),
 		ddns.UsingHTTPClient(http.DefaultClient),
 	)
 	if err != nil {
@@ -55,7 +56,6 @@ func ExampleWebResolver() {
 func ExampleRunDaemon() {
 	ddnsClient, err := ddns.New("dynamic-local-ip.example.com",
 		ddns.NewCloudflare(os.Getenv("CLOUDFLARE_ZONE_TOKEN")),
-		ddns.WithLogger(log.Default()),
 	)
 	if err != nil {
 		log.Fatalf("error creating ddns client: %s", err)
@@ -65,7 +65,6 @@ func ExampleRunDaemon() {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Hour)
 	defer cancel()
 	ddns.RunDaemon(ddnsClient, ctx, 5*time.Minute, nil)
-	<-ctx.Done()
 }
 func ExampleInterfaceResolver() {
 	resolver := ddns.InterfaceResolver("eth0", "wlan0")
@@ -84,14 +83,36 @@ func ExampleInterfaceResolver() {
 }
 
 func ExampleJoin() {
+	r := ddns.Join(
+		ddns.WebResolver("https://ipv4.icanhazip.com/"),
+		ddns.WebResolver("https://ipv6.icanhazip.com/"),
+	)
 	ddnsClient, err := ddns.New("dynamic-ip.example.com",
 		ddns.NewCloudflare(os.Getenv("CLOUDFLARE_ZONE_TOKEN")),
-		ddns.UsingResolver(
-			ddns.Join(
-				ddns.WebResolver("https://ipv4.icanhazip.com/"),
-				ddns.WebResolver("https://ipv6.icanhazip.com/"),
-			),
-		),
+		ddns.UsingResolver(r),
+	)
+	if err != nil {
+		log.Fatalf("error creating ddns client: %s", err)
+	}
+	// run once:
+	err = ddnsClient.RunDDNS(context.Background())
+	if err != nil {
+		log.Fatalf("ddns update failed: %s", err)
+	}
+}
+func ExampleResolverFunc() {
+	fn := func(ctx context.Context) ([]netip.Addr, error) {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-time.After(100 * time.Millisecond): // simulating some lookup method
+			ip, err := netip.ParseAddr("10.0.0.10")
+			return []netip.Addr{ip}, err
+		}
+	}
+	ddnsClient, err := ddns.New("dynamic-ip.example.com",
+		ddns.NewCloudflare(os.Getenv("CLOUDFLARE_ZONE_TOKEN")),
+		ddns.UsingResolver(ddns.ResolverFunc(fn)),
 	)
 	if err != nil {
 		log.Fatalf("error creating ddns client: %s", err)
